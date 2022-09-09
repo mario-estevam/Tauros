f3197db823022371d84b233c75a7304ec6f439bb
mario-estevam <mario.neto47@gmail.com>
2022-09-09 07:43:42 -0300

    testes datatable

--- src/main/resources/static/js/datatable.js
+++ src/main/resources/static/js/datatable.js
@@ -8,7 +8,9 @@ const status = {
 
 
 const tabItem1 = document.querySelector(status[params[2]]);
-console.log(params)
+
 const AllTabItem = document.querySelectorAll('#myTab a');
 AllTabItem.forEach((item) => item.classList.remove('active'));
 tabItem1.classList.add('active')
+
+
